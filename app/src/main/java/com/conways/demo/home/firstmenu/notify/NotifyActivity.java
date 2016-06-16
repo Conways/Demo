05487e6ecddda58c1b8b435c9545e448ffb00b6c
user <Conways@163.com>
2016-06-16 15:20:04 +0800

    Test push

--- app/src/main/java/com/conways/demo/home/firstmenu/notify/NotifyActivity.java
+++ app/src/main/java/com/conways/demo/home/firstmenu/notify/NotifyActivity.java
@@ -1,7 +1,15 @@
 package com.conways.demo.home.firstmenu.notify;
 
+import android.os.Bundle;
+
+import com.conways.demo.base.BaseTitleActivity;
+
 /**
  * Created by user on 2016/6/16.
  */
-public class NotifyActivity {
+public class NotifyActivity extends BaseTitleActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
