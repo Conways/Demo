60764562ef5d42b65cd72a83057301afd3634ff0
Conways <Conways@163.com>
2016-06-30 23:25:27 +0800

    完善linechart 用例

--- app/src/main/java/com/conways/demo/home/firstmenu/linechart/ChartLine.java
+++ app/src/main/java/com/conways/demo/home/firstmenu/linechart/ChartLine.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.List;
@@ -14,10 +15,10 @@ import java.util.List;
  */
 public class ChartLine extends View {
 
-    private int topPadding = px2Dp(60);
-    private int leftPadding = px2Dp(200);
-    private int bottomPadding = px2Dp(200);
-    private int rightPadding = px2Dp(60);
+    private int topPadding = dp2px(5);
+    private int leftPadding = dp2px(20);
+    private int bottomPadding = dp2px(20);
+    private int rightPadding = dp2px(5);
 
     private int mWith;
     private int mHeight;
@@ -35,12 +36,14 @@ public class ChartLine extends View {
     private Paint xTextPaint;
     private Paint yTextPaint;
     private Paint gridPaint;
+    private Paint pointBgPaint;
     private Paint pointPaint;
+    private Paint linePaint;
 
 
     private List<List<Data>> datas;
-    private int offSet=0;
-    private int moveoffset=0;
+    private int offSet = 0;
+    private int moveoffset = 0;
 
 
     public ChartLine(Context context) {
@@ -66,33 +69,49 @@ public class ChartLine extends View {
         paddingBgPaint.setColor(0xffffffff);
 
         xPaint = new Paint();
-        xPaint.setStrokeWidth(px2Dp(3));
+        xPaint.setStrokeWidth(dp2px(1));
         xPaint.setColor(0xffed6d35);
 
         yPaint = new Paint();
         yPaint.setColor(0xffed6d35);
-        yPaint.setStrokeWidth(px2Dp(3));
+        yPaint.setStrokeWidth(dp2px(1));
 
         xTextPaint = new Paint();
         xTextPaint.setColor(0xffed6d35);
-        xTextPaint.setTextSize(px2Dp(80));
+        xTextPaint.setTextSize(dp2px(10));
         xTextPaint.setAntiAlias(true);
 
         yTextPaint = new Paint();
         yTextPaint.setColor(0xffed6d35);
-        yTextPaint.setTextSize(px2Dp(80));
+        yTextPaint.setTextSize(dp2px(10));
         yTextPaint.setAntiAlias(true);
 
         gridPaint = new Paint();
-        gridPaint.setStrokeWidth(px2Dp(1));
+        gridPaint.setStrokeWidth(dp2px(0.5f));
         gridPaint.setColor(0x80ed6d35);
 
-        pointPaint=new Paint();
-        pointPaint.setColor(0xffed6d35);
+        pointBgPaint = new Paint();
+        pointBgPaint.setColor(0xffed6d35);
+        pointBgPaint.setAntiAlias(true);
+
+
+        pointPaint = new Paint();
+        pointPaint.setColor(0xffffffff);
         pointPaint.setAntiAlias(true);
+
+        linePaint=new Paint();
+        linePaint.setColor(0xffed6d35);
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(dp2px(0.5f));
     }
 
-
+    public void setDatas(List<List<Data>> datas) {
+        if (datas == null) {
+            return;
+        }
+        this.datas = datas;
+        invalidate();
+    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -107,20 +126,28 @@ public class ChartLine extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBg(canvas);
-        drawXY(canvas);
-        drawValue(canvas);
+        drawY(canvas);
         drawPoint(canvas);
+        drawHorizontalBg(canvas);
+        drawX(canvas);
+        drawValue(canvas);
     }
 
     private void drawBg(Canvas canvas) {
         canvas.drawRect(0, 0, mWith, topPadding, paddingBgPaint);
-        canvas.drawRect(0, topPadding, leftPadding, mHeight, paddingBgPaint);
         canvas.drawRect(0, mHeight - bottomPadding, mWith, mHeight, paddingBgPaint);
-        canvas.drawRect(leftPadding, topPadding, mWith, mHeight - bottomPadding, bgPaint);
+        canvas.drawRect(leftPadding, topPadding, mWith-rightPadding, mHeight - bottomPadding, bgPaint);
+    }
+    private void drawHorizontalBg(Canvas canvas) {
+        canvas.drawRect(0, topPadding, leftPadding, mHeight, paddingBgPaint);
+        canvas.drawRect(mWith-rightPadding, 0, mWith, mHeight, paddingBgPaint);
     }
 
-    private void drawXY(Canvas canvas) {
+
+    private void drawX(Canvas canvas) {
         canvas.drawLine(leftPadding, topPadding, leftPadding, mHeight - bottomPadding, xPaint);
+    }
+    private void drawY(Canvas canvas) {
         canvas.drawLine(leftPadding, mHeight - bottomPadding, mWith - rightPadding, mHeight - bottomPadding,
                 yPaint);
     }
@@ -145,23 +172,38 @@ public class ChartLine extends View {
             String value = i * yValueTemp + "";
             Rect rect = new Rect();
             xTextPaint.getTextBounds(value, 0, value.length(), rect);
-            int x = leftPadding - rect.width() - px2Dp(20);
+            int x = leftPadding - rect.width() - dp2px(3);
             int y = mHeight - i * verticalItem - bottomPadding + rect.height() / 2;
             canvas.drawText(i * yValueTemp + "", x, y, xTextPaint);
         }
 
     }
 
-    private void drawPoint(Canvas canvas){
-        canvas.drawCircle(mWith/2,mHeight/2,px2Dp(60),pointPaint);
+    private void drawPoint(Canvas canvas) {
 
-        if (datas==null){
+        if (datas == null) {
             return;
         }
 
-        for (int i = 0; i <datas.size() ; i++) {
-            for (int j = 0; j <datas.get(i).size(); j++) {
+        int horizontalItem = (mWith - leftPadding - rightPadding) / horizontalCont;
 
+        for (int i = 0; i < datas.size(); i++) {
+            for (int j = 0; j < datas.get(i).size(); j++) {
+                Data data = datas.get(i).get(j);
+                float x = mWith  + offSet + moveoffset - j * horizontalItem;
+                float y = topPadding + (mHeight - topPadding - bottomPadding) * (max - data.getValue()) /
+                        (max - min);
+
+                if (j < datas.get(i).size() - 1) {
+                    float x1 = mWith  + offSet + moveoffset - (j + 1) * horizontalItem;
+                    float y1 = topPadding + (mHeight - topPadding - bottomPadding) * (max - datas.get
+                            (i).get(j + 1).getValue()) / (max - min);
+                    canvas.drawLine(x, y, x1, y1, linePaint);
+                }
+
+
+                canvas.drawCircle(x, y, dp2px(4f), pointBgPaint);
+                canvas.drawCircle(x, y, dp2px(3f), pointPaint);
 
             }
         }
@@ -169,9 +211,14 @@ public class ChartLine extends View {
 
     }
 
-    private int px2Dp(float px) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    private int dp2px(float dp) {
         final float scale = getContext().getResources().getDisplayMetrics().density;
-        return (int) (px / scale + 0.5f);
+        return (int) (dp * scale + 0.5f);
     }
 
 }
--- app/src/main/java/com/conways/demo/home/firstmenu/linechart/LineChartActivity.java
+++ app/src/main/java/com/conways/demo/home/firstmenu/linechart/LineChartActivity.java
@@ -6,21 +6,48 @@ import android.view.View;
 import com.conways.demo.R;
 import com.conways.demo.base.BaseTitleActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by user on 2016/6/30.
  */
 public class LineChartActivity extends BaseTitleActivity implements View.OnClickListener{
+
+    private List<List<Data>> datas;
+    private ChartLine chartLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_linechart);
         super.onCreate(savedInstanceState);
         initTitle();
+        initData();
+        init();
     }
 
 
     private void initTitle(){
         tvTitleTitle.setText(getText(R.string.linechart_title));
+    }
+
+    private void initData(){
+        datas=new ArrayList<>();
+        for (int i = 0; i <3; i++) {
+            List<Data> list=new ArrayList<>();
+            for (int j = 0; j <10 ; j++) {
+                Data data =new Data();
+                int value=(int)(Math.random()*20)+i*40;
+                data.setValue(value);
+                list.add(data);
+            }
+            datas.add(list);
+        }
+    }
+
+    private void init() {
+        chartLine=(ChartLine)findViewById(R.id.activity_linechart_linechart);
+        chartLine.setDatas(datas);
     }
 
     @Override
